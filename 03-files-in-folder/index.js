fe93e2828a1284ad99d85fda0d43afc1db7af4aa
Ihor Salfetnyk <4el@ukr.net>
2024-01-24 21:26:15 +0200

    fix: fix Sync function

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,24 +1,27 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const folderPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
-  if (err) {
-    console.error(`Error reading directory: ${err.message}`);
-    return;
-  }
+async function readDirectory() {
+  try {
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
 
-  files.forEach((file) => {
-    if (file.isFile()) {
-      const filePath = path.join(folderPath, file.name);
-      const fileStats = fs.statSync(filePath);
-      
-      const fileName = path.parse(file.name).name;
-      const fileExtension = path.parse(file.name).ext.replace('.', '');
-      const fileSize = fileStats.size / 1024;
+    for (const file of files) {
+      if (file.isFile()) {
+        const filePath = path.join(folderPath, file.name);
+        const fileStats = await fs.stat(filePath);
+        
+        const fileName = path.parse(file.name).name;
+        const fileExtension = path.parse(file.name).ext.replace('.', '');
+        const fileSize = fileStats.size / 1024;
 
-      console.log(`${fileName} - ${fileExtension} - ${fileSize.toFixed(3)}kb`);
+        console.log(`${fileName} - ${fileExtension} - ${fileSize.toFixed(3)}kb`);
+      }
     }
-  });
-});
+  } catch (err) {
+    console.error(`Error reading directory: ${err.message}`);
+  }
+}
+
+readDirectory();
