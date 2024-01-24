0a39d9978c545a80ea7e654b82a1413737f44944
Ihor Salfetnyk <4el@ukr.net>
2024-01-24 22:07:49 +0200

    fix: fix 04-copy-directiry problem

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,19 +6,19 @@ const destinationFolder = path.join(__dirname, 'files-copy');
 
 async function copyDirectory() {
   try {
-    await fsPromises.access(sourceFolder, constants.F_OK);
+    await fsPromises.rm(destinationFolder, { force: true, recursive: true });
 
     await fsPromises.mkdir(destinationFolder, { recursive: true });
 
-    const files = await fsPromises.readdir(sourceFolder);
+    const files = await fsPromises.readdir(sourceFolder, { withFileTypes: true });
 
     for (const file of files) {
-      const sourceFilePath = path.join(sourceFolder, file);
-      const destinationFilePath = path.join(destinationFolder, file);
+      const fileStartPath = path.join(sourceFolder, file.name);
+      const fileDestinationFolder = path.join(destinationFolder, file.name);
 
-      await fsPromises.copyFile(sourceFilePath, destinationFilePath);
+      await fsPromises.copyFile(fileStartPath, fileDestinationFolder);
     }
-
+    
     console.log('Directory successfully copied.');
   } catch (err) {
     console.error(`Error when copying a directory: ${err.message}`);
